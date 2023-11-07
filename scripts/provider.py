4e27d7bc15521431298b4c25bdb21133b66641d0
Raouaaa <raouaaghodhbani@think-it.io>
2023-11-07 16:33:22 +0100

    Add make_api_request function

--- scripts/provider.py
+++ scripts/provider.py
@@ -3,10 +3,17 @@ from dotenv import load_dotenv
 import os
 
 
+def make_api_request(url, payload, api_key):
+    response = requests.post(url, json=payload, headers={"x-api-key": api_key})
+    response.raise_for_status()
+    response_json = response.json()
+    return response_json["@id"]
+
+
 def create_asset(base_url, api_key):
 
     asset_url = base_url+'/assets'
-    myobj = {
+    payload = {
         "@context": {
             "edc": "https://w3id.org/edc/v0.0.1/ns/"
         },
@@ -26,39 +33,14 @@ def create_asset(base_url, api_key):
             "secretAccessKey": secret_access_key
         }
     }
-    asset_response = requests.post(
-        asset_url, json=myobj, headers={"x-api-key": api_key})
-    if asset_response.status_code == 200:
-        asset_json = asset_response.json()
-        return asset_json["@id"]
-    else:
-        raise Exception(
-            f"Asset creation failed with status code {asset_response.status_code}")
-
-
-'''{
-  "@context": {
-    "edc": "https://w3id.org/edc/v0.0.1/ns/"
-  },
-  "policy": {
-    "@context": "http://www.w3.org/ns/odrl.jsonld",
-    "@type": "Set",
-    "uid": "example-policy-7000",
-    "permission": [
-      {
-        "target": "20be3a74-8aaf-4e7e-9458-5a0268414973",
-        "action": "USE",# Use or DISPLAY as env variable 
-        "constraint": []
-      }
-    ]
-  }
-}'''
+    id = make_api_request(asset_url, payload, api_key)
+    return id
 
 
 def create_policy(base_url, api_key, asset_id):
 
     policy_url = base_url+'/policydefinitions'
-    myobj = {
+    payload = {
         "@context": {
             "edc": "https://w3id.org/edc/v0.0.1/ns/"
         },
@@ -75,16 +57,8 @@ def create_policy(base_url, api_key, asset_id):
             ]
         }
     }
-    policy_response = requests.post(
-        policy_url, json=myobj, headers={"x-api-key": api_key})
-    # policy_json = policy_response.json()
-    # return (policy_json["@id"])
-    if policy_response.status_code == 200:
-        policy_json = policy_response.json()
-        return policy_json["@id"]
-    else:
-        raise Exception(
-            f"Asset creation failed with status code {policy_response.status_code}")
+    id = make_api_request(policy_url, payload, api_key)
+    return id
 
 
 def create_contract_definition(base_url, api_key, contract_definition_name, asset_id, policy_id):
@@ -105,16 +79,8 @@ def create_contract_definition(base_url, api_key, contract_definition_name, asse
                 "operandRight": asset_id
             }]
     }
-    contract_definition_response = requests.post(
-        contract_definition_url, json=myobj, headers={"x-api-key": api_key})
-    # contract_definition_json = contract_definition_response.json()
-    # return (contract_definition_json["@id"])
-    if contract_definition_response.status_code == 200:
-        contract_definition_json = contract_definition_response.json()
-        return contract_definition_json["@id"]
-    else:
-        raise Exception(
-            f"Asset creation failed with status code {contract_definition_response.status_code}")
+    id = make_api_request(contract_definition_url, myobj, api_key)
+    return id
 
 
 if __name__ == "__main__":
@@ -139,8 +105,5 @@ if __name__ == "__main__":
         base_url, api_key, contract_definition_name, asset_id, policy_id)
 
     print(asset_id)
-    print(type(asset_id))
     print(policy_id)
-    print(type(policy_id))
     print(contract_definition_id)
-    print(type(contract_definition_id))
