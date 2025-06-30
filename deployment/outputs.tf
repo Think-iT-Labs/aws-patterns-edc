ff32c284d3cfbd659099fc47b98a410f9720241e
mohamedkhalilbeldi <mohamedkhalilbeldi@think-it.io>
2025-06-30 15:30:20 +0100

    infra: add sensitive database credentials outputs for multi-company configuration

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -163,6 +163,23 @@ output "next_steps" {
 }
 
 ########################################
+# Database Configuration Information
+########################################
+
+output "database_passwords" {
+  description = "Database passwords for both companies (sensitive information)"
+  value = {
+    companyx_admin_password = var.companyx_datasource.admin_password
+    companyx_user = var.companyx_datasource.username
+    companyx_user_password  = var.companyx_datasource.password
+    companyy_admin_password = var.companyy_datasource.admin_password
+    companyy_user = var.companyy_datasource.username
+    companyy_user_password  = var.companyy_datasource.password
+  }
+  sensitive = true
+}
+
+########################################
 # Quick Access Information
 ########################################
 
